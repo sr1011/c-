09adf9ef3ff949b5c27e077bebeb473694124677
sakurairyo <ryo.s.1011.football@gmail.com>
2022-02-12 12:10:11 +0900

    c言語

---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main(){
+  printf("こんにちは。私の名前は%sです。¥n年齢は%d歳です。¥n","櫻井僚,26")
+}
---
+++ sample.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int n[] = { 5,4,3,2,1 };
+    int i;
+    for(i = 0; i < 5; i++){
+        printf("n[%d]=%d ",i,n[i]);
+    }
+    printf("¥n");
+}
