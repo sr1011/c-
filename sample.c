6a4630b68719eaad52a251931e345a05ebdea6cb
sakurairyo <ryo.s.1011.football@gmail.com>
2022-02-19 13:04:17 +0900

    変数wa

--- sample.c
+++ sample.c
@@ -8,7 +8,10 @@ int main(void)
   printf("整数n1："); scanf("%d",&n1);
   printf("整数n2："); scanf("%d",&n2);
 
-  printf("それらの和は%dです。\n",n1+n2);
+  int wa = n1 + n2;
+
+  printf("それらの和は%dです。\n",wa);
+  printf("すなわちn1 + n2 = %dです。\n", wa);
 
   return 0;
 }
