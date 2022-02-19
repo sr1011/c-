33ce08d2c4c4c4e839e1381cff01ce11db591b70
sakurairyo <ryo.s.1011.football@gmail.com>
2022-02-19 13:05:36 +0900

    演習1-7

--- sample.c
+++ sample.c
@@ -2,16 +2,10 @@
 
 int main(void)
 {
-  int n1, n2;
 
-  puts("2つの整数を入力してください。");
-  printf("整数n1："); scanf("%d",&n1);
-  printf("整数n2："); scanf("%d",&n2);
-
-  int wa = n1 + n2;
-
-  printf("それらの和は%dです。\n",wa);
-  printf("すなわちn1 + n2 = %dです。\n", wa);
+  puts("守");
+  puts("破");
+  puts("離");
 
   return 0;
 }
