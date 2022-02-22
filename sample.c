3007720fa4555e8eea0624df111b32d4c0c95937
sakurairyo <ryo.s.1011.football@gmail.com>
2022-02-22 21:44:13 +0900

    単項の算術演算子

--- sample.c
+++ sample.c
@@ -2,13 +2,12 @@
 
 int main(void)
 {
-  int a, b;
+  int num;
 
-  puts("二つの整数を入力せよ：");
-  printf("整数a："); scanf("%d",&a);
-  printf("整数b："); scanf("%d",&b);
-  
-  printf("それらの和は%dで積は%dです。\n", a + b, a * b);
+  puts("整数を入力せよ：");
+  scanf("%d",&num);
+
+  printf("%dの符号を反転させた値は%dです。\n", +num, -num);
 
   return 0;
 }
