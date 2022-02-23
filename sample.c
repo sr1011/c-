f14642f06993c5a1457123db52930714b252d3d2
sakurairyo <ryo.s.1011.football@gmail.com>
2022-02-23 15:09:12 +0900

    intとdouble

--- sample.c
+++ sample.c
@@ -2,16 +2,28 @@
 
 int main(void)
 {
-  double x, y;
+  int    n1, n2, n3, n4;
+  double d1, d2, d3, d4;
 
-  puts("整数を入力せよ：");
-  printf("整数x："); scanf("%lf",&x);
-  printf("整数y："); scanf("%lf",&y);
+  n1 = 15 /2;
+  n2 = 15.0 / 2.0;
+  n3 = 15.0 / 2;
+  n4 = 15 / 2.0;
 
-  printf("x + y = %f\n", x + y);
-  printf("x - y = %f\n", x - y);
-  printf("x * y = %f\n", x * y);
-  printf("x / y = %f\n", x / y);
-  
+  d1 = 15 /2;
+  d2 = 15.0 / 2.0;
+  d3 = 15.0 / 2;
+  d4 = 15 / 2.0;
+
+  printf("n1 = %d\n", n1);
+  printf("n2 = %d\n", n2);
+  printf("n3 = %d\n", n3);
+  printf("n4 = %d\n\n", n4);
+
+  printf("d1 = %f\n", d1);
+  printf("d2 = %f\n", d2);
+  printf("d3 = %f\n", d3);
+  printf("d4 = %f\n", d4);
+
   return 0;
 }
