e5c114762706d73b762de27c411005cd20613cbb
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-01 15:09:30 +0900

    演習3-4

--- sample.c
+++ sample.c
@@ -2,15 +2,18 @@
 
 int main(void)
 {
-  int num;
+  int n1, n2;
 
-  printf("整数を入力せよ：");
-  scanf("%d",&num);
+  printf("整数を入力せよ：\n");
+  printf("整数１："); scanf("%d",&n1);
+  printf("整数２："); scanf("%d",&n2);
 
-  if (num < 0)
-        num = -num;
-    
-    printf("絶対値は%dです。\n", num);
+  if (n1 == n2)
+    puts("AとBは等しいです。");
+  else if (n1 > n2)
+    puts("AはBより大きいです。");
+  else
+  puts("AはBより小さいです。");
   
   return 0;
 }
