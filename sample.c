1d34f0c9b6854ea446f846575fa16b58db467d7c
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-03 14:14:31 +0900

    演習3-7

--- sample.c
+++ sample.c
@@ -2,19 +2,21 @@
 
 int main(void)
 {
-  int n1, n2, n3;
+  int n1, n2, n3, n4;
 
   puts("整数を入力せよ");
   printf("整数１："); scanf("%d",&n1);
   printf("整数２："); scanf("%d",&n2);
   printf("整数３："); scanf("%d",&n3);
+  printf("整数４："); scanf("%d",&n4);
 
 
-  int min = n1;
-  if (min > n2) min = n2;
-  if (min > n3) min = n3;
+  int max = n1;
+  if (n2 > max) max = n2;
+  if (n3 > max) max = n3;
+  if (n4 > max) max = n4;
 
-   printf("最大値は%dです。\n", min);
+   printf("最大値は%dです。\n", max);
   
   return 0;
 }
