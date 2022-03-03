786ebff8cdbe5e192fd2c206b337ad2619733218
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-03 14:19:57 +0900

    簡易版

--- sample.c
+++ sample.c
@@ -8,8 +8,7 @@ int main(void)
   printf("整数１："); scanf("%d",&n1);
   printf("整数２："); scanf("%d",&n2);
 
-  int max = n1 > n2 ? n1 : n2;
-   printf("最大値は%dです。\n", max);
+  printf("最大値は%dです。\n", n1 > n2 ? n1 : n2);
   
   return 0;
 }
