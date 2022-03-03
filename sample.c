2b0f16b1c1ed6a0e9f294bd5db5060779cd74d9f
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-03 14:19:14 +0900

    条件演算子

--- sample.c
+++ sample.c
@@ -7,15 +7,8 @@ int main(void)
   puts("整数を入力せよ");
   printf("整数１："); scanf("%d",&n1);
   printf("整数２："); scanf("%d",&n2);
-  printf("整数３："); scanf("%d",&n3);
-  printf("整数４："); scanf("%d",&n4);
 
-
-  int max = n1;
-  if (n2 > max) max = n2;
-  if (n3 > max) max = n3;
-  if (n4 > max) max = n4;
-
+  int max = n1 > n2 ? n1 : n2;
    printf("最大値は%dです。\n", max);
   
   return 0;
