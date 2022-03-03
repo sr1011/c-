8c6f599b39e2bcaa3e72ae93faf0f26d05f97afc
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-03 14:10:18 +0900

    ３値の最大値

--- sample.c
+++ sample.c
@@ -2,15 +2,19 @@
 
 int main(void)
 {
-  int n1, n2;
+  int n1, n2, n3;
 
   puts("整数を入力せよ");
   printf("整数１："); scanf("%d",&n1);
   printf("整数２："); scanf("%d",&n2);
+  printf("整数３："); scanf("%d",&n3);
 
-  int max;
-  if (n1 > n2) max = n1; else max = n2;
-    printf("大きい方の値は%dです。\n", max);
+
+  int max = n1;
+  if (n2 > max) max = n2;
+  if (n3 > max) max = n3;
+
+   printf("最大値は%dです。\n", max);
   
   return 0;
 }
