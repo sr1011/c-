0ffd2139605fe1ade43446ac6e042a70d248a108
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-04 16:41:59 +0900

    演習3-9

--- sample.c
+++ sample.c
@@ -2,16 +2,13 @@
 
 int main(void)
 {
-  int n1, n2, n3, n4;
+  int n1, n2;
 
   puts("二つの整数を入力せよ");
   printf("整数１："); scanf("%d",&n1);
   printf("整数２："); scanf("%d",&n2);
 
- if (n1 > n2)
-  printf("その差は%dです。\n", n1 - n2);
- else
-  printf("その差は%dです。\n", n2 - n1);
-
+  n1 == n2 ? printf("それらの値は同じです。\n") : printf("それらの値は違います。\n");
+ 
   return 0;
 }
