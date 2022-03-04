971c83f25e635d2c7ad1ef9c206296d11f34c22a
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-04 16:27:33 +0900

    ２値の差

--- sample.c
+++ sample.c
@@ -4,11 +4,11 @@ int main(void)
 {
   int n1, n2, n3, n4;
 
-  puts("整数を入力せよ");
+  puts("二つの整数を入力せよ");
   printf("整数１："); scanf("%d",&n1);
   printf("整数２："); scanf("%d",&n2);
 
-  printf("最大値は%dです。\n", n1 > n2 ? n1 : n2);
+  printf("その差は%dです。\n", n1 > n2 ? n1 - n2 : n2 - n1);
   
   return 0;
 }
