7b54e6994322e7cddab7b2e54a3f2859bf38c8fc
sakurairyo <ryo.s.1011.football@gmail.com>
2022-03-04 16:29:52 +0900

    演習3-8

--- sample.c
+++ sample.c
@@ -8,7 +8,10 @@ int main(void)
   printf("整数１："); scanf("%d",&n1);
   printf("整数２："); scanf("%d",&n2);
 
-  printf("その差は%dです。\n", n1 > n2 ? n1 - n2 : n2 - n1);
-  
+ if (n1 > n2)
+  printf("その差は%dです。\n", n1 - n2);
+ else
+  printf("その差は%dです。\n", n2 - n1);
+
   return 0;
 }
